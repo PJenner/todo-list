fd642fa8e11d08641a912994daf3c7c3e21e0ce7
PJenner <47118828+PJenner@users.noreply.github.com>
2021-03-19 14:14:54 +0000

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,3 +1,5 @@
 sudo apt update 
 sudo apt install python3 python3-pip -y
 pip3 install pytest pytest-cov
+
+python3 -m pytest --doctest-modules --junitxml=junit/test-results.xml --cov=application --cov-report=xml --cov-report=html
